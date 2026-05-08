6271feeaef45936fd152c17d51c2603bf587e808
Dom Morgan <dom@d3r.com>
2026-05-08 15:06:03 +0100

    Add some more licences

--- .allowed-licenses.php
+++ .allowed-licenses.php
@@ -10,7 +10,10 @@ return (new LicenseConfigurationBuilder())
         'BSD-2-Clause',
         'BSD-3-Clause',
         'Apache-2.0',
-        // And other licenses you wish to allow.
+        'LGPL-3.0+',
+        'MPL-2.0',
+        'ISL',
+        'MPL-2.0',
     )
     ->addAllowedVendor('d3r') // Allow any license from a specific vendor, i.e. your own company.
     ->build();
