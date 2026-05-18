798d7840bfc344679755c00e2c5958a5f9d98c3f
Dom Morgan <dom@d3r.com>
2026-05-18 20:37:34 +0100

    Allow more LGPL versions

--- .allowed-licenses.php
+++ .allowed-licenses.php
@@ -10,6 +10,8 @@ return (new LicenseConfigurationBuilder())
         'BSD-2-Clause',
         'BSD-3-Clause',
         'Apache-2.0',
+        'LGPL-2.1-only',
+        'LGPL-2.1-or-later',
         'LGPL-3.0+',
         'MPL-2.0',
         'ISC',
