c434d3ad76f3499137e8f7afc2ed5b5e4e334e21
Dom Morgan <dom@d3r.com>
2026-05-26 11:09:37 +0100

    Facebook SDK doesn't correctly report its licence

--- .allowed-licenses.php
+++ .allowed-licenses.php
@@ -12,10 +12,12 @@ return (new LicenseConfigurationBuilder())
         'Apache-2.0',
         'LGPL-2.1-only',
         'LGPL-2.1-or-later',
+        'LGPL-3.0-only',
         'LGPL-3.0+',
         'LGPL-3.0-or-later',
         'MPL-2.0',
         'ISC',
     )
+    ->addAllowedPackage('facebook/php-business-sdk') // https://github.com/facebook/facebook-php-business-sdk/blob/main/LICENSE
     ->addAllowedVendor('d3r') // Allow all D3R packages
     ->build();
