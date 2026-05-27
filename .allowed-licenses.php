24325f70661c3db70b2c63734a656e5978457335
Dom Morgan <dom@d3r.com>
2026-05-27 12:23:09 +0100

    Add extra licence

--- .allowed-licenses.php
+++ .allowed-licenses.php
@@ -2,22 +2,6 @@
 
 declare(strict_types=1);
 
-use Lendable\ComposerLicenseChecker\LicenseConfigurationBuilder;
+use D3R\Standards\ComposerLicenseChecker;
 
-return (new LicenseConfigurationBuilder())
-    ->addLicenses(
-        'MIT',
-        'BSD-2-Clause',
-        'BSD-3-Clause',
-        'Apache-2.0',
-        'LGPL-2.1-only',
-        'LGPL-2.1-or-later',
-        'LGPL-3.0-only',
-        'LGPL-3.0+',
-        'LGPL-3.0-or-later',
-        'MPL-2.0',
-        'ISC',
-    )
-    ->addAllowedPackage('facebook/php-business-sdk') // https://github.com/facebook/facebook-php-business-sdk/blob/main/LICENSE
-    ->addAllowedVendor('d3r') // Allow all D3R packages
-    ->build();
+return ComposerLicenseChecker::defaultBuilder()->build();
